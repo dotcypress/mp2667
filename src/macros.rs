45a98e06acddc3e00a2c3eb5f28be036e4e40857
Vitaly Domnikov <dotcypress@gmail.com>
2022-01-15 15:12:26 -0800

    cleanup

--- src/macros.rs
+++ src/macros.rs
@@ -3,7 +3,7 @@ use crate::*;
 pub trait ReadOnlyRegister: From<u8> {
     const ADDR: u8;
 
-    fn read<E, I2C: WriteRead<Error = E>>(i2c: &mut I2C) -> Result<Self, E> {
+    fn read<E, I2C: i2c::WriteRead<Error = E>>(i2c: &mut I2C) -> Result<Self, E> {
         let buf = &mut [0u8; 1];
         i2c.write_read(I2C_ADDR, &[Self::ADDR], buf)
             .map(|_| buf[0].into())
@@ -17,7 +17,7 @@ impl<RWR: ReadWriteRegister> ReadOnlyRegister for RWR {
 pub trait ReadWriteRegister: From<u8> + Into<u8> {
     const ADDR: u8;
 
-    fn write<E, I2C: Write<Error = E>>(self, i2c: &mut I2C) -> Result<(), E> {
+    fn write<E, I2C: i2c::Write<Error = E>>(self, i2c: &mut I2C) -> Result<(), E> {
         i2c.write(I2C_ADDR, &[Self::ADDR, self.into()])
     }
 }
---
+++ src/registers.rs
@@ -0,0 +1,245 @@
+use crate::*;
+
+register_map!(
+    InputSourceControl: 0x00, RW,
+    PowerOnConfiguration: 0x01, RW,
+    ChargeCurrentControl: 0x02, RW,
+    DischargeAndTerminationCurrent: 0x03, RW,
+    ChargeVoltageControl: 0x04, RW,
+    ChargeTerminationAndTimerControl: 0x05, RW,
+    MiscellaneousOperationControl: 0x06, RW,
+    SystemStatus: 0x07, RO,
+    FaultFlags: 0x08, RO,
+);
+
+#[derive(BitfieldSpecifier)]
+pub enum ChargeStatus {
+    NotCharging,
+    PreCharge,
+    Charge,
+    ChargeDone,
+}
+
+#[bitfield]
+pub struct SystemStatus {
+    #[skip(setters)]
+    pub thermal_regulation: bool,
+    #[skip(setters)]
+    pub power_good: bool,
+    #[skip(setters)]
+    pub power_path_enabled: bool,
+    #[skip(setters)]
+    pub charge_status: ChargeStatus,
+    #[skip(setters)]
+    pub revision: B2,
+    #[skip]
+    __: bool,
+}
+
+#[bitfield]
+pub struct FaultFlags {
+    #[skip]
+    __: B2,
+    #[skip(setters)]
+    pub safety_timer_expired: bool,
+    #[skip(setters)]
+    pub battery_fault: bool,
+    #[skip(setters)]
+    pub thermal_shutdown: bool,
+    #[skip(setters)]
+    pub input_fault: bool,
+    #[skip(setters)]
+    pub watchdog_timer_expired: bool,
+    #[skip]
+    __: bool,
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum ThermalThreshold {
+    T60C,
+    T80C,
+    T100C,
+    T120C,
+}
+
+#[bitfield]
+pub struct MiscellaneousOperationControl {
+    pub thermal_regulation_threshold: ThermalThreshold,
+    #[skip]
+    __: bool,
+    pub ntc_enabled: bool,
+    #[skip]
+    __: bool,
+    pub battery_fet_disabled: bool,
+    pub extended_safety_timer: bool,
+    #[skip]
+    __: bool,
+}
+
+impl Default for MiscellaneousOperationControl {
+    fn default() -> Self {
+        Self {
+            bytes: [0b0000_1011],
+        }
+    }
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum SafetyTimerPeriod {
+    P20h,
+    P5h,
+    P8h,
+    P12h,
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum WatchdogTimerLimit {
+    Disabled,
+    L40s,
+    L80s,
+    L160s,
+}
+
+#[bitfield]
+pub struct ChargeTerminationAndTimerControl {
+    pub termination_control_enabled: bool,
+    pub timer_period: SafetyTimerPeriod,
+    pub timer_enabled: bool,
+    pub timer_limit: WatchdogTimerLimit,
+    pub termination_enabled: bool,
+    #[skip]
+    __: bool,
+}
+
+impl Default for ChargeTerminationAndTimerControl {
+    fn default() -> Self {
+        Self {
+            bytes: [0b0100_1010],
+        }
+    }
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum RechargeThreshold {
+    U150mV,
+    U300mV,
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum PrechargeThreshold {
+    U2800mV,
+    U3000mV,
+}
+
+#[bitfield]
+pub struct ChargeVoltageControl {
+    pub recharge_threshold: RechargeThreshold,
+    pub precharge_threshold: PrechargeThreshold,
+    pub regulation_voltage: B6,
+}
+
+impl Default for ChargeVoltageControl {
+    fn default() -> Self {
+        Self {
+            bytes: [0b1010_0011],
+        }
+    }
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum TerminalCurrent {
+    I24mA,
+    I52mA,
+    I80mA,
+    I108mA,
+}
+
+#[bitfield]
+pub struct DischargeAndTerminationCurrent {
+    pub terminal_current: TerminalCurrent,
+    #[skip]
+    __: bool,
+    pub discharge_current_limit: B4,
+    #[skip]
+    __: bool,
+}
+
+impl Default for DischargeAndTerminationCurrent {
+    fn default() -> Self {
+        Self {
+            bytes: [0b0100_1001],
+        }
+    }
+}
+
+#[bitfield]
+pub struct ChargeCurrentControl {
+    pub charge_current: B5,
+    #[skip]
+    __: B3,
+}
+
+impl Default for ChargeCurrentControl {
+    fn default() -> Self {
+        Self {
+            bytes: [0b0000_0111],
+        }
+    }
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum UVLOThreshold {
+    U2400mV,
+    U2500mV,
+    U2600mV,
+    U2700mV,
+    U2800mV,
+    U2900mV,
+    U3000mV,
+    U3100mV,
+}
+
+#[bitfield]
+pub struct PowerOnConfiguration {
+    pub uvlo_threshold: UVLOThreshold,
+    pub charge_disabled: bool,
+    #[skip]
+    __: B2,
+    pub watchdog_timer_reset: bool,
+    pub settings_reset: bool,
+}
+
+impl Default for PowerOnConfiguration {
+    fn default() -> Self {
+        Self {
+            bytes: [0b0000_0100],
+        }
+    }
+}
+
+#[derive(BitfieldSpecifier)]
+pub enum InputCurrentLimit {
+    I77mA,
+    I118mA,
+    I345mA,
+    I470mA,
+    I540mA,
+    I635mA,
+    I734mA,
+    I993mA,
+}
+
+#[bitfield]
+pub struct InputSourceControl {
+    pub input_current_limit: InputCurrentLimit,
+    pub input_minimum_voltage: B4,
+    pub ldo_fet_disabled: bool,
+}
+
+impl Default for InputSourceControl {
+    fn default() -> Self {
+        Self {
+            bytes: [0b0100_1011],
+        }
+    }
+}
